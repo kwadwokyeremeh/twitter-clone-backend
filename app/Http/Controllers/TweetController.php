326395218893309e07583dd07263ea1245a1582a
Kwadwo Kyeremeh Tuffour <kyerematics@gmail.com>
2023-07-17 15:39:29 +0000

    Pull data for single tweet screen

---
+++ app/Http/Controllers/TweetController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTweetRequest;
+use App\Http\Requests\UpdateTweetRequest;
+use Illuminate\Http\Request;
+use App\Models\Tweet;
+
+class TweetController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Tweet::with('user:id,name,username,avatar')->latest()->paginate(10);
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTweetRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Tweet $tweet)
+    {
+        return $tweet->load('user:id,name,username,avatar');
+        //return Tweet::with('user:id,name,username,avatar')->where('id', $tweet->id)->first();
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Tweet $tweet)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTweetRequest $request, Tweet $tweet)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Tweet $tweet)
+    {
+        //
+    }
+}
---
+++ database/factories/TweetFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Tweet>
+ */
+class TweetFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'body' => $this->faker->sentence(20),
+            'created_at' => $this->faker->dateTimeBetween('-6 months', now()),
+        ];
+    }
+}
